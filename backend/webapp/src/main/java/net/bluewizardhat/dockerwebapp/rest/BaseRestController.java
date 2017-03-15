7887d0fa23221d719dfc42fbda1be7073ad458f1
BlueWizardHat <BlueWizardHat@users.noreply.github.com>
2017-03-15 18:45:39 +0100

    simplify baserestcontroller async handling

--- backend/webapp/src/main/java/net/bluewizardhat/dockerwebapp/rest/BaseRestController.java
+++ backend/webapp/src/main/java/net/bluewizardhat/dockerwebapp/rest/BaseRestController.java
@@ -7,8 +7,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.SettableListenableFuture;
 import org.springframework.web.context.request.async.DeferredResult;
 
 
@@ -16,40 +14,34 @@ public class BaseRestController {
 
 	private ExecutorService executorService = Executors.newCachedThreadPool();
 
-	private static class ListenableFutureAdaptor<T> extends DeferredResult<T> {
-		public ListenableFutureAdaptor(ListenableFuture<T> future) {
-			future.addCallback(this::setResult, this::setErrorResult);
-		}
-	}
-
 	protected <T> DeferredResult<T> submit(final Callable<T> callable) {
-		final SettableListenableFuture<T> future = new SettableListenableFuture<>();
+		final DeferredResult<T> deferredResult = new DeferredResult<>();
 
 		executorService.execute(wrapRunnable(() -> {
 			try {
 				T result = callable.call();
-				future.set(result);
+				deferredResult.setResult(result);
 			} catch (Exception e) {
-				future.setException(e);
+				deferredResult.setErrorResult(e);
 			}
 		}));
 
-		return new ListenableFutureAdaptor<>(future);
+		return deferredResult;
 	}
 
 	protected DeferredResult<Void> execute(final Runnable runnable) {
-		final SettableListenableFuture<Void> future = new SettableListenableFuture<>();
+		final DeferredResult<Void> deferredResult = new DeferredResult<>();
 
 		executorService.execute(wrapRunnable(() -> {
 			try {
 				runnable.run();
-				future.set(null);
+				deferredResult.setResult(null);
 			} catch (Exception e) {
-				future.setException(e);
+				deferredResult.setErrorResult(e);
 			}
 		}));
 
-		return new ListenableFutureAdaptor<>(future);
+		return deferredResult;
 	}
 
 	private Runnable wrapRunnable(final Runnable runnable) {
