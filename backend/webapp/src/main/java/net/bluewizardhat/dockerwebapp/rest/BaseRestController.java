3212d2ab9a6e48b7a2554b75dcf7148c90d1d0db
BlueWizardHat <BlueWizardHat@users.noreply.github.com>
2017-03-18 14:45:15 +0100

    javadoc

--- backend/webapp/src/main/java/net/bluewizardhat/dockerwebapp/rest/BaseRestController.java
+++ backend/webapp/src/main/java/net/bluewizardhat/dockerwebapp/rest/BaseRestController.java
@@ -6,15 +6,35 @@ import static net.bluewizardhat.dockerwebapp.util.concurrent.RunnableWrappers.wr
 import java.util.concurrent.Callable;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.async.DeferredResult;
 
+import lombok.extern.slf4j.Slf4j;
 import net.bluewizardhat.dockerwebapp.util.concurrent.WebExecutorService;
 
+@Slf4j
 public abstract class BaseRestController {
 
 	@Autowired
 	private WebExecutorService executorService;
 
+	@ExceptionHandler(IllegalArgumentException.class)
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public void handleException(IllegalArgumentException e) {
+		log.warn("Unhandled exception: " + e.getMessage(), e);
+	}
+
+	@ExceptionHandler(Exception.class)
+	@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+	public void handleException(Exception e) {
+		log.warn("Unhandled exception: " + e.getMessage(), e);
+	}
+
+	/**
+	 * Executes the given Callable in the background and returns a DeferredResult for the result.
+	 */
 	protected <T> DeferredResult<T> submit(final Callable<T> callable) {
 		final DeferredResult<T> deferredResult = new DeferredResult<>();
 
@@ -30,6 +50,9 @@ public abstract class BaseRestController {
 		return deferredResult;
 	}
 
+	/**
+	 * Executes the given Runnable in the background and returns a DeferredResult for the result.
+	 */
 	protected DeferredResult<Void> execute(final Runnable runnable) {
 		final DeferredResult<Void> deferredResult = new DeferredResult<>();
 
