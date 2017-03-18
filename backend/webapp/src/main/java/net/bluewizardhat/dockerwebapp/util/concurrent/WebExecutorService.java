40478bd31f6adeea756b1d22af15b70dae63b131
BlueWizardHat <BlueWizardHat@users.noreply.github.com>
2017-03-18 14:44:51 +0100

    more configurable webexecutorservice

--- backend/webapp/src/main/java/net/bluewizardhat/dockerwebapp/util/concurrent/WebExecutorService.java
+++ backend/webapp/src/main/java/net/bluewizardhat/dockerwebapp/util/concurrent/WebExecutorService.java
@@ -6,30 +6,70 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * A wrapper around an {@linkplain ExecutorService} for web methods.
+ */
 @Slf4j
 @Component
 public class WebExecutorService {
 
-	@Value("${maxThreadsPrProcessor:4}")
+	/**
+	 * If larger than zero this is the maximum number of threads that will be allocated
+	 * to run background tasks. Otherwise use <code>maxThreadsPrProcessor</code> instead.
+	 */
+	@Value("${webapp.maxThreads:-1}")
+	private int maxThreads;
+
+	/**
+	 * Maximum number of threads per processor that will be allocated to run background tasks.
+	 * If <code>maxThreads</code> is set this will be ignored.
+	 */
+	@Value("${webapp.maxThreadsPrProcessor:4}")
 	private int maxThreadsPrProcessor;
 
 	private ExecutorService executorService;
 
 	@PostConstruct
 	private void postConstruct() {
-		int availableProcessors = Runtime.getRuntime().availableProcessors();
-		int maxThreads = availableProcessors * maxThreadsPrProcessor;
-		executorService = new ThreadPoolExecutor(0, maxThreads, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
-		log.info("WebExecutorService initialized with availableProcessors={}, maxThreadsPrProcessor={}, maxThreads={}", availableProcessors, maxThreadsPrProcessor, maxThreads);
+		if (maxThreads > 0) {
+			executorService = newCachedThreadPool(maxThreads);
+			log.info("WebExecutorService initialized with maxThreads={}", maxThreads);
+		} else {
+			int availableProcessors = Runtime.getRuntime().availableProcessors();
+			int calculatedMaxThreads = availableProcessors * maxThreadsPrProcessor;
+			executorService = newCachedThreadPool(calculatedMaxThreads);
+			log.info("WebExecutorService initialized with availableProcessors={}, maxThreadsPrProcessor={}, maxThreads={}", availableProcessors, maxThreadsPrProcessor, calculatedMaxThreads);
+		}
 	}
 
+	@PreDestroy
+	private void preDestroy() {
+		if (executorService != null) {
+			executorService.shutdown();
+		}
+	}
+
+	/**
+	 * A wrapper for {@linkplain ExecutorService#execute(Runnable)}
+	 * @see ExecutorService#execute(Runnable)
+	 */
 	public void execute(Runnable runnable) {
 		executorService.execute(runnable);
+	}
+
+	/**
+	 * Like <code>Executors.newCachedThreadPool()</code> but with a maximal number of threads.
+	 *
+	 * @see java.util.concurrent.Executors#newCachedThreadPool()
+	 */
+	private ExecutorService newCachedThreadPool(int maxThreads) {
+		return new ThreadPoolExecutor(0, maxThreads, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
 	}
 }
