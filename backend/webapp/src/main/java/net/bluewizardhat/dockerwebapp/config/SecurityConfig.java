2668c4d663c59a4e4e247b3ee1c1d331b2b9c1b3
BlueWizardHat <BlueWizardHat@users.noreply.github.com>
2017-04-24 10:46:28 +0200

    moved security config

--- backend/webapp/src/main/java/net/bluewizardhat/dockerwebapp/config/SecurityConfig.java
+++ backend/webapp/src/main/java/net/bluewizardhat/dockerwebapp/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package net.bluewizardhat.dockerwebapp.web;
+package net.bluewizardhat.dockerwebapp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
