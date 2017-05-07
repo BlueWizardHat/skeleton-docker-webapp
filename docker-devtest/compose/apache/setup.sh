58e0c5ee0e57a92d00bcd8401f13e4bd947dd9ac
BlueWizardHat <BlueWizardHat@users.noreply.github.com>
2017-05-07 16:19:29 +0200

    fix for locale-gen missing from recent ubuntu image

--- docker-devtest/compose/apache/setup.sh
+++ docker-devtest/compose/apache/setup.sh
@@ -3,6 +3,9 @@
 echo "* Updating cache"
 apt-get update
 
+echo "* Installing locales package"
+apt-get install -y locales
+
 echo "* Installing apache and supervisor"
 apt-get install -y apache2 supervisor
 
@@ -15,4 +18,5 @@ a2enmod proxy_ajp
 
 cp /setup/supervisord.conf /etc/supervisord.conf
 
+# Add UTF-8 encoding support in the image
 locale-gen en_US.UTF-8  
