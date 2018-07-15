e1ed9e204bc871fef716d62e765d8d0efe5fa1fc
BlueWizardHat <BlueWizardHat@users.noreply.github.com>
2018-07-15 11:08:13 +0200

    updated dependencies

--- backend/webapp/src/main/java/net/bluewizardhat/dockerwebapp/web/Application.java
+++ backend/webapp/src/main/java/net/bluewizardhat/dockerwebapp/web/Application.java
@@ -1,11 +1,7 @@
 package net.bluewizardhat.dockerwebapp.web;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -18,12 +14,12 @@ public class Application {
     /**
      * Create a servlet container that serves ajp requests, so it can be served by apache
      */
-    @Bean
-    public EmbeddedServletContainerFactory ajpConnector() {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-        Connector ajpConnector = new Connector("org.apache.coyote.ajp.AjpNioProtocol");
-        ajpConnector.setPort(8009);
-        tomcat.addAdditionalTomcatConnectors(ajpConnector);
-        return tomcat;
-    }
+//    @Bean
+//    public EmbeddedServletContainerFactory ajpConnector() {
+//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+//        Connector ajpConnector = new Connector("org.apache.coyote.ajp.AjpNioProtocol");
+//        ajpConnector.setPort(8009);
+//        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+//        return tomcat;
+//    }
 }
