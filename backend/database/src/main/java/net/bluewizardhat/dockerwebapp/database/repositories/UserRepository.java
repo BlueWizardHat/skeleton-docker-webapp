13d2d778fdedc5051f483928a658b5625b25a5a6
BlueWizardHat <BlueWizardHat@users.noreply.github.com>
2019-04-02 20:21:14 +0200

    findById is now in the PagingAndSortingRepository

--- backend/database/src/main/java/net/bluewizardhat/dockerwebapp/database/repositories/UserRepository.java
+++ backend/database/src/main/java/net/bluewizardhat/dockerwebapp/database/repositories/UserRepository.java
@@ -2,11 +2,12 @@ package net.bluewizardhat.dockerwebapp.database.repositories;
 
 import java.util.Optional;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import net.bluewizardhat.dockerwebapp.database.entities.User;
 
 @Repository
-public interface UserRepository extends ExtendedPagingAndSortingRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 	public Optional<User> findByUserName(String userName);
 }
